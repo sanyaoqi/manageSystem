5a8a83290efaf0f431f6cc85c8b98993cc3ff8ea
xi <xi@xideMacBook-Pro.local>
2018-03-26 22:50:57 +0800

    delete gitignore

--- yiiars/backend/views/layouts/main.php
+++ yiiars/backend/views/layouts/main.php
@@ -25,20 +25,20 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/js/bootstrap/bootstrap.min.css">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="/bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/js/font-awesome/font-awesome.min.css">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="/bower_components/Ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="/js/Ionicons/ionicons.min.css">
     <!-- jvectormap -->
-    <link rel="stylesheet" href="/bower_components/jvectormap/jquery-jvectormap.css">
+    <link rel="stylesheet" href="/js/jvectormap/jquery-jvectormap.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="/dist/css/AdminLTE.min.css">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="/dist/css/skins/_all-skins.min.css">
     <!-- jQuery 3 -->
-    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="/js/jquery.min.js"></script>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -92,20 +92,20 @@ AppAsset::register($this);
 </footer>
 
 <!-- Bootstrap 3.3.7 -->
-<script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/js/bootstrap//bootstrap.min.js"></script>
 <!-- FastClick -->
-<script src="/bower_components/fastclick/lib/fastclick.js"></script>
+<script src="/js/fastclick/fastclick.js"></script>
 <!-- AdminLTE App -->
 <script src="/dist/js/adminlte.min.js"></script>
 <!-- Sparkline -->
-<script src="/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="/js/jquery-sparkline/jquery.sparkline.min.js"></script>
 <!-- jvectormap  -->
-<script src="/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="/js/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="/js/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 <!-- SlimScroll -->
-<script src="/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="/js/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- ChartJS -->
-<script src="/bower_components/Chart.js/Chart.js"></script>
+<script src="/js/Chart.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="/dist/js/pages/dashboard2.js"></script>
 <!-- AdminLTE for demo purposes -->
